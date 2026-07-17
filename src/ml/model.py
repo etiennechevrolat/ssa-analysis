257041733780faefdb359b8fdb02805f215c3ecd
etiennechevrolat <etienne.chevrolat@polytechnique.edu>
2026-07-17 11:21:42 +0200

    added a build_model fonction in model

--- src/ml/model.py
+++ src/ml/model.py
@@ -3,13 +3,12 @@ from torch import nn
 ### on a en entrée un tenseur (B,F,W) = (batch_size, features, window_size).
 ## pour le moment on veut dim=2 en sortie : manoeuvre EW/NS ? 
 class NaiveBaseLine(nn.Module):
-    def __init__(self, n_features, window_size, n_outputs):
+    def __init__(self, n_features, window_size):
         super().__init__()
         self.n_features = n_features
         self.window_size = window_size
         self.dim_embedding = n_features*window_size
-        self.n_outputs= n_outputs
-        self.layer1 = nn.Linear(self.dim_embedding, n_outputs) 
+        self.layer1 = nn.Linear(self.dim_embedding, 2) 
 
     def forward(self, x):
         batch_size, features, window_size = x.shape ## (B, F, W) 
@@ -19,4 +18,10 @@ class NaiveBaseLine(nn.Module):
         return x
 
 
+
+def build_model(cfg, *, n_features, window_size):
+    """cfg.model. aiguille vers le bon modèle selon cfg.name"""
+    if cfg.name == "naive_baseline":
+        return NaiveBaseLine(n_features, window_size)
+    raise KeyError (f"modèle inconnu : {cfg.name}")
 
--- src/ml/train.py
+++ src/ml/train.py
@@ -10,8 +10,7 @@ from omegaconf import DictConfig
 
 from ml.datahandler import load_splid_objects
 from ml.dataset import make_loaders
-from ml.model import NaiveBaseLine
-
+from ml.model import build_model
 
 
 def train_one_epoch(
@@ -25,47 +24,55 @@ def train_one_epoch(
     model.train()
     running_loss = 0.0
     total = 0
+    correct = 0
     for time_series_batch, labels in data_loader:
+        # time_series_batch: (B,F,W), labels (B,2)
         x = time_series_batch.to(device)
         y = labels.to(device)
-
+        print(labels)
         optimizer.zero_grad()
 
         pred = model(x)
 
+        correct += int((pred == labels).sum().item())
         loss = loss_fn(pred, y)
         running_loss+= float(loss.item()) * labels.size(0)
         loss.backward()
         optimizer.step()
         total += labels.size(0)
-    avg_loss = running_loss / (max(total, 1))
-    return avg_loss
+    avg_loss = running_loss / total
+    accuracy = correct/total
+    return avg_loss, accuracy
 
-@torch.no_grad()
+
+@torch.no_grad() ## décorateur, applique torch.no_grad(evaluate_epoch(...)) et coupe le suivi des gradients.
 def evaluate_epoch(
     model,
     data_loader,
     loss_fn, 
     device
     ):
-    model.eval()
+    model.eval() ## différents de torch.no_grad, change le comportement de certaines couches : Dropout, BatchNorm.
     running_loss = 0.0
     total=0
+    correct = 0 
     for time_series_batch, labels in data_loader:
         x = time_series_batch.to(device)
         y = labels.to(device)
 
         pred = model(x)
-
+        correct += int((pred == labels).sum().item())
         loss = loss_fn(pred, y)
         running_loss+= float(loss.item()) * labels.size(0)
-       
+    
 
         total += labels.size(0)
-    avg_loss = running_loss / (max(total, 1))
-    return avg_loss
+    avg_loss = running_loss / total
+    accuracy = correct/total
+    return avg_loss, accuracy
 
-@hydra.main(version_base=None, config_path="../../configs/ml", config_name="config")
+@hydra.main(version_base=None, config_path="../../configs/ml", config_name="config") 
+## hydra prend main en point d'entrée, et main() est transformé en wrapper qui récupère et construit l'objet cfg, et enfin applique main(cfg).
 def main(cfg : DictConfig):
 
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
@@ -84,22 +91,22 @@ def main(cfg : DictConfig):
         future=cfg.data.future,
         val_split=cfg.data.val_split,
         seed= cfg.seed)
+    ## On calcule le nombre de features considérées et la taille de la fenêtre temporelle
+    n_features = len(meta["features_cols"])
+    window_size  = cfg.history + cfg.future +1 
 
-    model = NaiveBaseLine(
-        cfg.model.n_features,
-        cfg.model.window_size, 
-        cfg.model.n_outputs)
+    model = build_model(cfg.model, n_features, window_size)
     model.to(device)
 
     params = model.parameters()
     optimizer = torch.optim.AdamW(params, lr = cfg.train.lr)
-    
+
     loss_fn = nn.BCEWithLogitsLoss()
 
     for epoch in range(cfg.train.epochs):
-        tr = train_one_epoch(model, train_loader, loss_fn, optimizer, device)
-        va = evaluate_epoch(model, val_loader, loss_fn, device)
-        print(f"epoch {epoch} : train {tr:.4f} val {va:.4f}")
+        train_loss, train_acc = train_one_epoch(model, train_loader, loss_fn, optimizer, device)
+        val_loss, val_acc = evaluate_epoch(model, val_loader, loss_fn, device)
+        print(f"epoch {epoch} : train loss/acc : {train_loss:.4f} | {train_acc} val loss/acc : {val_loss:.4f}  | {val_acc}")
 
 if __name__ == "__main__":
     main()
