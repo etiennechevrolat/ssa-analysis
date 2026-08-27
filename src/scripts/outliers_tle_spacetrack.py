b58dae4ae1ee11dc6c418ba37378cd0330990c70
etiennechevrolat <etienne.chevrolat@polytechnique.edu>
2026-08-27 15:18:24 +0200

    wip laptop avant sync symlink

--- src/scripts/outliers_tle_spacetrack.py
+++ src/scripts/outliers_tle_spacetrack.py
@@ -18,6 +18,9 @@ def load_data(data_dir) :
     return df
 df = load_data(data_dir)
 
+## les outliers_problématiques sont surtout ceux du semi grand axe. 
+## on va faire une passe de nettoyage brutale: 
+# détection sur une fenetre de 256 TLE des outliers des points qui sont loin de mean +- 3 sigma ET isolés.
 def plot_sma_local(norad, df, start, end):
     """
     Start/end sont des entiers correspondant au numéro des TLE depuis le premier TLE du dataframe de ce norad
@@ -37,5 +40,22 @@ def plot_sma_local(norad, df, start, end):
             ylabel= "sma")
         plt.show()
 
+def outliers_detection(norad, df, start, end, n_from_sigma = 3, plot = False):
+    df_norad = df[df['norad'] == norad]
+    sma= (df_norad['sma'].values).iloc[start:end]
+    mu = sma.mean()
+    sigma = sma.std()
+
+    outliers = [a for a in sma if (np.abs(a-mu)> n_from_sigma)]
+    if plot :
+        t = np.arange(start, end)
+        fig, ax = plt.subplots(figsize=(10,5))
+        x = [a for a in sma if (a not in outliers)]
+        ax.scatter(t, sma, marker ='.')
+        ax.set(title = f"Sma for norad {norad}", 
+            xlabel= "index tle",
+            ylabel= "sma")
+        plt.show()
+    return outliers 
 if __name__ == "__main__" : 
     plot_sma_local(56126, df, 0, 256)
